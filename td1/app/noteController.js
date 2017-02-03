ff0a2ad56dff01dfd8d0e64d891eb10254a37983
xavierefremont <xaviere.fremont@yahoo.fr>
2017-02-03 11:13:25 +0100

    Exercice 1
    Affichage info
    Sans statut, sans css

--- td1/app/noteController.js
+++ td1/app/noteController.js
@@ -4,11 +4,16 @@
 
 angular.module("appNote", ["ngCookies"]).controller("NoteController", [ '$cookies', function($cookies) {
     this.messageNote=$cookies.get('msg');
+    this.info="";
     this.clear = function () {
-        this.messageNote="";
+        if(this.messageNote!=""){
+            this.messageNote="";
+            this.info="";
+        }
     };
     this.save = function () {
         $cookies.put('msg',this.messageNote);
+        this.info="note sauvegardée";
     };
     this.count= function () {
         var nbMsg = 100-this.messageNote.length;
