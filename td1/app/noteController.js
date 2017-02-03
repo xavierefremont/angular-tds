ab9af001ef8689ec473a469ee57a64d4df79884a
xavierefremont <xaviere.fremont@yahoo.fr>
2017-02-03 10:48:10 +0100

    exercice 1
    Affichage de la longueur
    Effacer et sauvegarder

--- td1/app/noteController.js
+++ td1/app/noteController.js
@@ -2,16 +2,16 @@
  * Created by Xaviere on 03/02/2017.
  */
 
-angular.module("appNote", ["ngCookies"]).controller("NoteController", [ '$cookies', function ($cookies) {
-    this.messageNote=$cookies.get('msgN');
+angular.module("appNote", ["ngCookies"]).controller("NoteController", [ '$cookies', function($cookies) {
+    this.messageNote=$cookies.get('msg');
     this.clear = function () {
         this.messageNote="";
-    }
+    };
     this.save = function () {
-        $cookies.put('msgN',this.messageNote);
-    }
+        $cookies.put('msg',this.messageNote);
+    };
     this.count= function () {
         var nbMsg = 100-this.messageNote.length;
         return nbMsg;
-    }
+    };
 }]);
